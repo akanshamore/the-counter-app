1a3a541197f94cbab4666472e4d11dd89211e1c3
akanshamore <akanshamore21@gmail.com>
2023-03-31 19:04:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+
+import './App.css';
+import React from 'react';
+
+class Header extends React.Component {
+
+  render() {
+
+    return (
+
+      <div>
+
+        <h1> {this.props.title}</h1>
+      </div>
+    );
+  }
+}
+
+class Counter extends React.Component {
+
+  render() {
+
+    return (
+
+
+      <div>
+
+        <h1>{this.props.counter}</h1>
+
+      </div>
+    );
+  }
+}
+
+
+class ButtonsContainer extends React.Component {
+
+  render() {
+
+    return (
+
+
+      <div>
+
+        <button onClick={this.props.handleAdd}>+</button>
+        <button onClick={this.props.handleSubtract}>-</button>
+        <button onClick={this.props.handleReset}>Reset</button>
+
+      </div>
+    );
+  }
+}
+class App extends React.Component {
+
+  state = { counter: 0 }
+
+  handleAdd = () => {
+
+    this.setState({ counter: this.state.counter + 1 })
+
+  }
+
+  handleSubtract = () => {
+
+    this.setState({ counter: this.state.counter - 1 })
+  }
+  handleReset = () => {
+
+    this.setState({ counter: 0 })
+  }
+  render() {
+
+    const title = "The Counter App"
+    return (
+      <div>
+
+
+        <Header title={title} />
+        <Counter counter={this.state.counter} />
+        <ButtonsContainer
+          handleAdd={this.handleAdd} handleSubtract={this.handleSubtract} handleReset={this.handleReset} />
+
+      </div>
+
+    );
+  }
+}
+export default App;
